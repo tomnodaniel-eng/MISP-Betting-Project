831d3ea3f9659c3738e1f930885b3e593009df9c
tomnodaniel-eng <tomnodaniel@gmail.com>
2025-10-03 22:18:03 +0300

    ci: fix flake8 style issues in tests

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,12 +1,15 @@
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from math_utils import add, multiply
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
 
 def test_multiply():
     assert multiply(2, 3) == 6
