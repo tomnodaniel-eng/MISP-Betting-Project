d6cf8213828bac99e22baaff5d24f4543fcc1c80
tomnodaniel-eng <tomnodaniel@gmail.com>
2025-10-03 22:23:32 +0300

    ci: fix flake8 import and line length issues

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Add repo root to path for tests
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from math_utils import add, multiply
 
